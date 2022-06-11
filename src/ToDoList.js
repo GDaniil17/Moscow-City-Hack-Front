16781f238282545712262b9cfc7b44f4c9764fb8
Daniil <galimovdv@yandex.ru>
2022-06-11 13:06:45 +0300

    Removed extra files

--- src/ToDoList.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ToDo from "./ToDo";
-
-export default function ToDoList({ todos, togleTodo }) {
-  return todos.map((todo) => {
-    return <ToDo key={todo.id} todo={todo} togleTodo={togleTodo} />;
-  });
-}
