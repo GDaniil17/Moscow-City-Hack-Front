183d106175af9e51e5455272537c1a94a45e63a0
Daniil <galimovdv@yandex.ru>
2022-06-11 12:13:15 +0300

    Current version

---
+++ src/Item.js
@@ -0,0 +1,31 @@
+import React, { PureComponent } from 'react';
+import picture from './imgs/sc.jpg';
+import styled from 'styled-components';
+import "./App.css";
+
+export default styled.div`
+    display: flex;
+    background-size: cover;
+    justify-content: center;
+    align-items: center;
+    height: 250px;
+    width: 100%;
+    color: #fff;
+    background-color: #00008B;
+    margin: 0 15px;
+    font-size: 200%;
+    textShadowColor: black;
+    textShadowOffset: {width: -1, height: 1};`;
+
+
+/*
+export default function Item ({}) {
+      return (
+        <div className='item' style={
+            {background-image: url(https://images.unsplash.com/photo-1542060748-10c28b62716f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)}}></div>
+      )
+  }
+*/
+
+
+
---
+++ src/ToDo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function ToDo( {todo, togleTodo}) {
+  function handleTodoClick() {
+    console.log(todo.id)
+    togleTodo(todo.id)
+  }
+  return (
+    <div>
+
+        <label>
+            <input type = "checkbox" checked={todo.complete} onChange = {handleTodoClick}/>
+            {todo.name}
+        </label>
+    </div>
+  )
+}
