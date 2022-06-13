7a94f33cf0f020cd2ff7cd9694290522bc7894c8
Daniil <galimovdv@yandex.ru>
2022-06-13 07:16:45 +0300

    Last version

--- src/APIHelper.js
+++ src/APIHelper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import "./App.css";
 
 async function APIHelper () {
-  const url = `http://213.178.155.140:4201/api/products/productsByCompany?id=4&count=100`;
+  const url = `http://84.252.138.236:4201/api/products/productsByCompany?id=4&count=100`;
 
   const response = await axios.get(url).catch((error) => {
     console.log(error);
